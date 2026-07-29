8ad6d798844901855ce05a9a84e2e6219185679b
Mahima Singh <mahima8081singh@gmail.com>
2026-07-29 15:36:10 +0530

    Bulk sync: 2 submissions

--- Data Structures & Algorithms/is-anagram/submission-0.cpp
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -4,9 +4,9 @@ public:
         if (s.length() != t.length()) {
             return false;
         }
-
-        sort(s.begin(), s.end());
+        sort(s.begin(),s.end ());
         sort(t.begin(), t.end());
         return s == t;
     }
 };
+        
